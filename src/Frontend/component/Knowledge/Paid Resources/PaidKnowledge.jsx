a66c3d58726acce8ceb18b4eb685b4de94e544d6
mahaveer <mahaveermnadloi9@gmail.com>
2024-06-19 15:37:10 +0530

    19th

--- src/Frontend/component/Knowledge/Paid Resources/PaidKnowledge.jsx
+++ src/Frontend/component/Knowledge/Paid Resources/PaidKnowledge.jsx
@@ -1,10 +1,37 @@
+// import React from "react";
+// import PaidResources from "../PaidResources";
+
+// const PaidKnowledge = () => {
+
+//   return (
+//     <>
+//       <PaidResources />
+//     </>
+//   );
+// };
+
+// export default PaidKnowledge;
+
 import React from "react";
 import PaidResources from "../PaidResources";
 
 const PaidKnowledge = () => {
+ 
+  const overlayStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    zIndex: 9999,
+    pointerEvents: "none", // Allows clicks to pass through to elements below
+  };
+
 
   return (
     <>
+      <div style={overlayStyle}></div>
+
       <PaidResources />
     </>
   );
