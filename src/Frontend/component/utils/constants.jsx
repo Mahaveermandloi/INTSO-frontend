052c54854124903f2a94c2084da6864424e7fd3e
mahaveer <mahaveermnadloi9@gmail.com>
2024-06-20 17:48:19 +0530

    20th

--- src/Frontend/component/utils/constants.jsx
+++ src/Frontend/component/utils/constants.jsx
@@ -1,12 +1,12 @@
 
-export const IP_ADDRESS = "192.168.1.2";
+// export const IP_ADDRESS = "192.168.1.2";
 
 // export const IP_ADDRESS = "localhost";
-//export const IP_ADDRESS = "192.168.1.7";
+// export const IP_ADDRESS = "192.168.1.7";
 
-export const PORT = "8000";
+// export const PORT = "8000";
 
-//  export const IP_ADDRESS = "49.13.221.129";
-//  export const PORT = "3000";
+ export const IP_ADDRESS = "49.13.221.129";
+ export const PORT = "3000";
 
 export const API_KEY = "ajeet";
